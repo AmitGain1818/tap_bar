6488c60b7e2df6997056582c5c5e3562df02425e
amitgain1818 <sampamondal75@gmail.com>
2022-01-10 16:56:22 +0600

    Add Image

--- lib/main.dart
+++ lib/main.dart
@@ -18,11 +18,11 @@ class TabBarApp extends StatelessWidget {
             backgroundColor: Colors.teal,
             bottom: const TabBar(
               tabs: [
-                Tab(icon: Icon(Icons.directions_train)),
                 Tab(icon: Icon(Icons.directions_walk)),
                 Tab(icon: Icon(Icons.directions_bike)),
                 Tab(icon: Icon(Icons.directions_car)),
                 Tab(icon: Icon(Icons.phone_iphone)),
+                Tab(icon: Icon(Icons.photo_album)),
               ],
             ),
             title: const Text('Tabs'),
@@ -30,11 +30,6 @@ class TabBarApp extends StatelessWidget {
           ),
           body: const TabBarView(
             children: [
-              Icon(
-                Icons.directions_train,
-                size: 400,
-                color: Colors.teal,
-              ),
               Icon(
                 Icons.directions_walk,
                 size: 400,
@@ -55,6 +50,11 @@ class TabBarApp extends StatelessWidget {
                 size: 400,
                 color: Colors.teal,
               ),
+              Image(
+                image: NetworkImage(
+                    'https://images.unsplash.com/photo-1593642632559-0c6d3fc62b89?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=869&q=80'),
+                fit: BoxFit.fitWidth,
+              )
             ],
           ),
         ),
